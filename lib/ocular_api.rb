c5cb3787eb540da538be966dbb17dcc897f6e191
Yomi Colledge <baphled@boodah.net>
2013-06-19 13:36:40 +0100

    Keep commits message on 1 line

--- lib/ocular_api.rb
+++ lib/ocular_api.rb
@@ -7,8 +7,7 @@ class OcularApi < Sinatra::Base
   get '/deploys.txt' do
     deploy = Ocular::Deploys.new name: Ocular::Config.deploy_project, stage: Ocular::Config.deploy_stage
     last_deployment = deploy.last_deployment
-    "Revision ##{last_deployment.short_revision} of #{Ocular::Config.deploy_project} was #{last_deployment.status} deployed to #{Ocular::Config.deploy_stage} by
-    #{last_deployment.user} on #{last_deployment.date} and finished at #{last_deployment.finished_at}".capitalize
+    "Revision ##{last_deployment.short_revision} of #{Ocular::Config.deploy_project} was #{last_deployment.status} deployed to #{Ocular::Config.deploy_stage} by #{last_deployment.user} on #{last_deployment.date} and finished at #{last_deployment.finished_at}".capitalize
   end
 
   get '/commits.json' do
